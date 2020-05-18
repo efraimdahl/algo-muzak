209b7d8a03ec910ebeb219ca7b6c414b54ab1cf2
Efraim Dahl <efraimdahl@uchicago.edu>
2020-05-18 10:52:41 -0500

    inserted a playable sine wave keyboard, mostly copied but with slight adjustments

--- js/main.js
+++ js/main.js
@@ -2,13 +2,48 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)()
 const fft = new AnalyserNode(ctx, { fftSize: 2048 })
 createWaveCanvas({ element: 'section', analyser: fft })
 
+var keyboard = new QwertyHancock({
+  id: 'keyboard',
+  width: innerWidth/2,
+  height: innerHeight/3,
+  octaves: 2,
+  startNote: 'A3',
+  whiteNotesColour: '#9e0035',
+  blackNotesColour: '#9e0035',
+  activeColour: '#9e0035',
+  borderColour: '#9e0035'
+})
 
-/*
+nodes = [];
 
-  const tone = new OscillatorNode(ctx)
-  tone.connect(fft)
-  tone.connect(ctx.destination)
-  tone.start(ctx.currentTime)
-  tone.stop(ctx.currentTime + 1)
+masterGain = ctx.createGain();
+nodes = [];
 
-*/
+masterGain.gain.value = 0.3;
+masterGain.connect(ctx.destination); 
+
+keyboard.keyDown = function (note, frequency) {
+    var oscillator = ctx.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.value = frequency;
+    oscillator.start(0);
+    oscillator.connect(masterGain);
+    oscillator.connect(fft);
+    nodes.push(oscillator);
+};
+
+
+keyboard.keyUp = function (note, frequency) {
+    var new_nodes = [];
+
+    for (var i = 0; i < nodes.length; i++) {
+        if (Math.round(nodes[i].frequency.value) === Math.round(frequency)) {
+            nodes[i].stop(0);
+            nodes[i].disconnect();
+        } else {
+            new_nodes.push(nodes[i]);
+        }
+    }
+
+    nodes = new_nodes;
+};
